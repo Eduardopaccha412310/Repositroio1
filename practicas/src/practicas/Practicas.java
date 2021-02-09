67e3f8a64aee364aed0263941516c932abb54aa5
Paccha <Paccha@DESKTOP-JT8L05N>
2021-02-09 09:45:49 -0500

    mi aporte numero 2

---
+++ practicas/src/practicas/Practicas.java
@@ -0,0 +1,12 @@
+
+package practicas;
+import javax.swing.JOptionPane;
+public class Practicas {
+
+    public static void main(String[] args) {
+        metodoobtjeto persona = new metodoobtjeto();
+        
+JOptionPane.showMessageDialog(null,"Eres hermosaaa!!");
+    }
+    
+}
---
+++ practicas/src/practicas/metodoobtjeto.java
@@ -0,0 +1,52 @@
+
+package practicas;
+
+public class metodoobtjeto {
+    private int id,numcelular; 
+    private String nombre; 
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the numcelular
+     */
+    public int getNumcelular() {
+        return numcelular;
+    }
+
+    /**
+     * @param numcelular the numcelular to set
+     */
+    public void setNumcelular(int numcelular) {
+        this.numcelular = numcelular;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+
+    
+}
